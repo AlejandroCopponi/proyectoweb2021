d5a80e0bbdd69fd70cae9fd6f973ef5c794fb4a9
AlejandroCopponi <82058590+AlejandroCopponi@users.noreply.github.com>
2021-06-13 15:12:00 -0300

    formulario de contacto

---
+++ js/mio.js
@@ -0,0 +1,10 @@
+// formulario de contacto en nueva pestaña minimizada
+window.onload = function() {
+    var anchor = document.getElementById("anchor");         
+    anchor.onclick = function() {
+        
+        var url = "contacto.html";
+        window.open(url, "_blank", 'width=750rem,height=482rem'); 
+        return false;
+    };
+}
