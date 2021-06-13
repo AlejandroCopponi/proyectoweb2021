5dc66f970c0460154f1574c957b5d8cd359be1f1
AlejandroCopponi <82058590+AlejandroCopponi@users.noreply.github.com>
2021-06-13 18:20:17 -0300

    iconos y fles de contact

--- js/mio.js
+++ js/mio.js
@@ -1,10 +1,32 @@
+//cambio de menú al scrollear
+ window.onscroll = function() {scrollFunction()};
+ function scrollFunction() {
+   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+     document.getElementById("navbar").style.background = "rgba(0, 0, 0, 0.750)";
+   } else {
+     document.getElementById("navbar").style.background = "rgba(0, 0, 0, 0.100)";
+   }
+ }
+
+  //AOS.init();// inicialización básica
+  AOS.init({
+    easing: 'ease-out-back',
+    duration: 1500
+});
+
 // formulario de contacto en nueva pestaña minimizada
 window.onload = function() {
     var anchor = document.getElementById("anchor");         
     anchor.onclick = function() {
-        
         var url = "contacto.html";
-        window.open(url, "_blank", 'width=750rem,height=500rem'); 
+        window.open(url, "_blank", 'width=750rem,height=650rem'); 
         return false;
     };
 }
+
+//función para cerra ventana botón cerrar formulario contacto
+var miVentana; 
+        function cerrarVentana() { 
+            miVentana = window 
+            miVentana.close();  
+        } 
