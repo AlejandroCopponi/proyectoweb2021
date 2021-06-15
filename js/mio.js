dc50736ed6048cb41966e959c78b80c164e90d60
AlejandroCopponi <82058590+AlejandroCopponi@users.noreply.github.com>
2021-06-15 19:51:21 -0300

    php a htlm

--- js/mio.js
+++ js/mio.js
@@ -19,7 +19,7 @@ window.onload = function() {
     var anchor = document.getElementById("anchor");         
     anchor.onclick = function() {
         var url = "contacto.html";
-        window.open(url, "_blank", 'width=750rem,height=650rem'); 
+        window.open(url, "_blank", 'width=750rem,height=700rem'); 
         return false;
     };
 }
@@ -30,3 +30,27 @@ var miVentana;
             miVentana = window 
             miVentana.close();  
         } 
+
+//funciones para la galería
+const $owlCarousel = $(".owl-carousel").owlCarousel({
+  items: 1,
+  loop: true,
+  nav: true,
+  navText: [
+    '<svg width="50" height="50" viewBox="0 0 24 24"><path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z"/></svg>',
+    '<svg width="50" height="50" viewBox="0 0 24 24"><path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z"/></svg>'
+  ]
+});
+
+$($owlCarousel).on("changed.owl.carousel", e => {
+  $(".owl-slide-animated").removeClass("is-transitioned");
+  const $currentOwlItem = $(".owl-item").eq(e.item.index);
+  $currentOwlItem.find(".owl-slide-animated").addClass("is-transitioned");
+});
+
+/*you have to bind initialized event before owl's initialization (see demo) */
+$(".owl-carousel").on("initialized.owl.carousel", () => {
+  setTimeout(() => {
+    $(".owl-item.active .owl-slide-animated").addClass("is-transitioned");
+  }, 200);
+});
